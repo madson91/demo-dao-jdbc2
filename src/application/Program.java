9337e868b4c39ec78b53797af74ff490b5a591db
Madson Cardoso dos Santos basto <madson91@gmail.com>
2019-07-19 11:49:17 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,13 @@ public class Program {
 		
 		Seller objS = new Seller(1,"madson","madson@gmail",new Date(), 300.00,obj);
 		System.out.println(objS);
+		
+		SellerDao dao = DaoFactory.createSellerDao();
+		
+		Seller seller = dao.findById(7);
+		System.out.println(seller);
+		
+		
 	}
 
 }
