bacbe092af4b5fed703b6c8fac209fb29551c795
Madson Cardoso dos Santos basto <madson91@gmail.com>
2019-07-19 11:11:32 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 
 		Department obj  = new Department(1, "teste");
 		System.out.println(obj);
-
+		
+		Seller objS = new Seller(1,"madson","madson@gmail",new Date(), 300.00,obj);
+		System.out.println(objS);
 	}
 
 }
